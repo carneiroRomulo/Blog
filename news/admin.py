761b46b3c912997a79d5fc3407850811856cea84
Romulo Carneiro <romulo.carneirodk@gmail.com>
2022-07-23 11:13:39 -0300

    feature: Add News model

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, News
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', )
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'title', 'category',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(News, NewsAdmin)
--- news/models.py
+++ news/models.py
@@ -1,3 +1,62 @@
+import uuid
+
+from django.contrib.auth.models import User
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(
+        max_length=20,
+        validators=[
+            MinLengthValidator(6),
+            MaxLengthValidator(20),
+        ]
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class News(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    cover = models.ImageField(upload_to='news/covers/%Y/%m/%d/')
+
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    title = models.CharField(
+        max_length=64,
+        validators=[
+            MinLengthValidator(12),
+            MaxLengthValidator(64),
+        ]
+    )
+    description = models.CharField(
+        max_length=100,
+        validators=[
+            MinLengthValidator(0),
+            MaxLengthValidator(100),
+        ]
+    )
+    body = models.CharField(
+        max_length=5000,
+        validators=[
+            MinLengthValidator(100),
+            MaxLengthValidator(5000),
+        ]
+    )
+    body_is_html = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "News"
